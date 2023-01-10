323ab77c2a293ed079888c16d100dcce24847553
Bruce Clark <bruce.clark55@gmail.com>
2023-01-10 17:16:16 -0600

    jokes

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,31 @@
+import { useState } from 'react';
+import { Button } from 'react-bootstrap';
+import getJoke from '../api/jokeData';
+import Joker from '../components/Joker';
+
 function Home() {
+  const [btnText, setBtnText] = useState('Get A Joke');
+  const [joke, setJoke] = useState({});
+
+  const setButton = (text) => {
+    setBtnText(text);
+  };
+  const getAJoke = () => {
+    getJoke().then((obj) => {
+      setJoke({
+        setup: obj.setup,
+        delivery: obj.delivery,
+      });
+      setButton('Get Punchline');
+    });
+  };
+
   return (
-    <div
-      className="text-center d-flex flex-column justify-content-center align-content-center"
-      style={{
-        height: '90vh',
-        padding: '30px',
-        maxWidth: '400px',
-        margin: '0 auto',
-      }}
-    >
-      <h1>Welcome Home!</h1>
+    <div>
+      <Joker joke={joke} btnText={btnText} />
+      {btnText === 'Get A Joke' || btnText === 'Get Another Joke'
+        ? <Button type="button" onClick={getAJoke}> {btnText}</Button>
+        : <Button type="button" onClick={() => setButton('Get Another Joke')}> </Button>}
     </div>
   );
 }
